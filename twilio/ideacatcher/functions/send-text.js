956af42aac5c9a12827c5f4f4d879eb0ea13feb7
LukeDasios <luke.dasios321@gmail.com>
2022-06-28 20:27:27 -0400

    My voicemail message should be sent to me as a SMS

--- twilio/ideacatcher/functions/send-text.js
+++ twilio/ideacatcher/functions/send-text.js
@@ -1,3 +1,18 @@
+require("dotenv").config();
+
+const ACCOUNT_SID = process.env.ACCOUNT_SID;
+const AUTH_TOKEN = process.env.AUTH_TOKEN;
+const PHONE_NUMBER = process.env.PHONE_NUMBER;
+const client = require("twilio")(ACCOUNT_SID, AUTH_TOKEN);
+
 exports.handler = (context, event, callback) => {
-  callback(null, "console check")
-}
+  client.messages
+    .create({
+      body: `Transcription: ${event.TranscriptionText}`,
+      from: PHONE_NUMBER,
+      to: "+15558675310"
+    })
+    .then((message) => console.log(message.sid));
+
+  callback(null, "console check");
+};
